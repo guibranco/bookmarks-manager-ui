838fadd30e337f78930cec44f8cc697b0b34ecbe
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-03-03 14:57:12 +0000

    Update sampleData.ts

--- src/data/sampleData.ts
+++ src/data/sampleData.ts
@@ -1,5 +1,5 @@
 import { Bookmark, Folder } from "../types";
-import sampleData from "../sample.json";
+import sampleData from "./sample.json";
 
 export const sampleFolders: Folder[] = sampleData.folders;
 export const sampleBookmarks: Bookmark[] = sampleData.bookmarks;
